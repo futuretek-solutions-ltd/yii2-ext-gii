7832bee23b2451e45ec39c25d2a534ae24869ae8
Petr Leo Compel <petr.compel@futuretek.cz>
2015-11-10 02:54:39 +0100

    normal crud generator Krajee add to form

--- crud/form.php
+++ crud/form.php
@@ -1,7 +1,7 @@
 <?php
 /* @var $this yii\web\View */
 /* @var $form yii\widgets\ActiveForm */
-/* @var $generator yii\gii\generators\crud\Generator */
+/* @var $generator futuretek\gii\generators\crud\Generator */
 
 echo $form->field($generator, 'modelClass');
 echo $form->field($generator, 'searchModelClass');
@@ -10,6 +10,7 @@ echo $form->field($generator, 'viewPath');
 echo $form->field($generator, 'baseControllerClass');
 echo $form->field($generator, 'indexWidgetType')->dropDownList([
     'grid' => 'GridView',
+    'krajeegrid' => 'Krajee GridView',
     'list' => 'ListView',
 ]);
 echo $form->field($generator, 'enableI18N')->checkbox();
